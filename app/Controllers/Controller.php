f577538d152b6952f3495a3abb1520d88fa6bda0
Garreau christèle <christele.garreau@sfr.fr>
2019-05-29 10:57:20 +0400

    Ajout fonctions login(), logout()

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -14,4 +14,26 @@ abstract class Controller{
         $content = ob_get_clean();  
         require '../Views/template.php';
     }
+    
+    public function login(){
+        if ($_REQUEST['pseudo'] ==="jean" && $_REQUEST['motDePass'] === 'bonjour' || $_REQUEST['pseudo'] ==="christ" && $_REQUEST['motDePass'] === 'bonjour' || $_REQUEST['pseudo'] ==="david" && $_REQUEST['motDePass'] === 'bonjour')
+        {header('Location:/edit');
+            if (session_status() === PHP_SESSION_NONE) {
+                session_start();
+            }
+            $_SESSION['user'] = $_REQUEST['pseudo'];
+            
+        }else{
+            header('Location:/login?erreur=password');
+        }
+    }
+    public function logout(){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+            $_SESSION = array();
+            unset($_SESSION['user']);;
+        }
+        //isset($_SESSION['user']);
+        header('Location:/');
+    }
 }
