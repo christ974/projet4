5e806faf96147cd41a4fe6c428856239e38f3f83
Garreau christèle <christele.garreau@sfr.fr>
2019-05-29 12:06:21 +0400

    Mise à jour du fichier

--- Views/supprimer.php
+++ Views/supprimer.php
@@ -1,38 +1,32 @@
-<?php 
-  session_start();
-?>
-<?php
-  ob_start(); 
-?>
-
 <div class="container">
 
-  <h1>Supprimer un chapitre</h1>
+    <h1>Supprimer un chapitre</h1>
 
-  <h3>Liste des chapitres</h3>
+    <h3>Liste des chapitres</h3>
 
-  <p><small class="pull-right"><a href="edit">Retour à la page administration</a> </small><br></p>
+    <?php
+        while ($req =$arts->fetch()){
+    ?>
 
-  <?php
-    while ($req =$arts->fetch()){
-  ?> 
+    <form method="post" action="/supprimer/<?= $req['id'] ?>" id="form_suppr">
+        <ul>
+            <li><label for="titre"><?=htmlspecialchars($req['titre'])?></label></li>
+        </ul>
 
-  <form method="post" action="/supprimer/<?= $req['id'] ?>" id="form_suppr">
-    <ul>
-      <li><label for="titre"><?=htmlspecialchars($req['titre'])?></label></li>
-    </ul>  
-        
-      <input type="submit" name="supprimer" id="supprimer" value="Supprimer">
-      <br><br>
-  </form>
-        
-  <?php
-    }
-    $arts->closeCursor();
-  ?>
+        <input type="submit" name="supprimer" id="supprimer" value="Supprimer">
+        <br><br>
+    </form>
+
+    <?php
+        }
+        $arts->closeCursor();
+    ?>
 
 </div>
-<?php
-  $content = ob_get_clean();
-  require('../Views/template.php');
-?>
+
+<div>
+    <form action="/logout" method="post" id="deconnexion" class="deconnexion">
+        <input type="submit" value="Déconnexion">
+    </form>
+
+</div>
