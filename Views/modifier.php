8d061c05700b902780cb513f690aa81a2c6930b8
Garreau christèle <christele.garreau@sfr.fr>
2019-05-29 12:04:33 +0400

    Mise à jour du fichier

--- Views/modifier.php
+++ Views/modifier.php
@@ -1,50 +1,40 @@
-<?php 
-session_start();
-?>
-<?php
-  ob_start(); 
-?>
-
-<!--<a href="/edit">Retourner à la page Administration</a>-->
-
 <div class="container">
 
-  <h1>Lire et modifier un chapitre</h1>
+    <h1>Modifier un chapitre</h1>
 
-  <p><small class="pull-right"><a href="edit">Retour à la page administration</a> </small><br></p>
+    <h3>Liste des chapitres</h3>
 
-  <h3>Liste des chapitres</h3>
-  
-  <?php 
-    while ($donnees = $artis->fetch()) {
-  ?>
-      
+    <?php 
+        while ($donnees = $artis->fetch()) {
+    ?>
 
-  <form method="post" action="/modifier/<?= $donnees['id'] ?>" id="form_modif">
 
-    <div class="form-group">
-      <label for="titre"><?=htmlspecialchars($donnees['titre'])?></label>
-      <input type="text" class="form-control" name="titre"  placeholder="Entrez votre titre">
-    </div>
-    <br> 
-    <div class="form-group">
-      <textarea id="mytextarea" class='mytextarea' name="contenu">
-        <?=  $donnees['contenu']. '<br>' ?>
-      </textarea>
-    </div>
-    <br>
-      <input type="submit" name="modifier" id="modifier" value="Modifier">
-    <br><br>
+    <form method="post" action="/modifier/<?= $donnees['id'] ?>" id="form_modif">
 
-  </form>
-     
-  <?php
+        <div class="form-group">
+            <label for="titre"><?=htmlspecialchars($donnees['titre'])?></label>
+            <input type="text" class="form-control" name="titre" placeholder="N'oubliez pas d'entrer votre titre" value="<?=htmlspecialchars($donnees['titre'])?>">
+        </div>
+        <br>
+        <div class="form-group">
+            <textarea id="mytextarea" name="contenu">
+                <?=  $donnees['contenu']. '<br>' ?>
+             </textarea>
+        </div>
+        <br>
+        <input type="submit" name="modifier" id="modifier" value="Modifier">
+        <br><br>
+
+    </form>
+
+    <?php
     }
     $artis->closeCursor(); // Termine le traitement de la requête
   ?>
 </div>
 
-<?php
-  $content = ob_get_clean();
-  require('../Views/template.php');
-?>
+<div>
+    <form action="/logout" method="post" id="deconnexion" class="deconnexion">
+        <input type="submit" value="Déconnexion">
+    </form>
+</div>
