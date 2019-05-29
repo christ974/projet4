559c4f2cd439fb6ecbc3daf751e22d6dc07bb3d0
Garreau christèle <christele.garreau@sfr.fr>
2019-05-29 11:12:35 +0400

    Mise à jour du fichier

--- Views/creer.php
+++ Views/creer.php
@@ -1,16 +1,10 @@
-<?php 
-session_start();
-?>
-<?php
-    ob_start(); 
-?>
-
 <div class="container">
 
     <h1>Création d'un chapitre</h1>
     <br>
-    <p><small class="pull-right"><a href="edit">Retour à la page administration</a> </small><br></p>
+
     <form method="post" action="/creer" id="form_creer">
+
         <div class="form-group">
             <label for="titre">Numéro du chapitre :</label>
             <input type="text" class="form-control" name="titre" placeholder="Entrez votre titre">
@@ -21,15 +15,14 @@ session_start();
             <textarea id="mytextarea" name="contenu"></textarea>
         </div>
         <input type="submit" id="envoyer" placehorder="Envoyer">
-        
+
     </form>
-    
+
 </div>
 
-<?php
-    /**
-     * var $content contient tt ce qui est affiché
-     */
-    $content = ob_get_clean();
-    require('../Views/template.php');
-?>
+<div>
+    <form action="/logout" method="post" id="deconnexion" class="deconnexion">
+        <input type="submit" value="Déconnexion">
+    </form>
+
+</div>
