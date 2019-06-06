40a0444ef33dd64364fbbbd80072da67c127c71a
Garreau christèle <christele.garreau@sfr.fr>
2019-06-06 14:01:28 +0400

    Ajout de commentaires

--- app/Models/ArticleManager.php
+++ app/Models/ArticleManager.php
@@ -25,7 +25,7 @@ class ArticleManager extends ModelManager
         return $article;
     }
 
-/**création d'un chapitre */
+    /**création d'un chapitre */
     public function create($titre, $contenu)
     {
         $chap = $this->bdd->prepare('INSERT INTO articles (titre, contenu, date_creation) VALUES(?, ?,  NOW())');
@@ -34,7 +34,7 @@ class ArticleManager extends ModelManager
         return $chap;
     }
 
-/**récup du dernier chapitre */
+    /**récup du dernier chapitre */
     public function lastArticleId(){
         $req = $this->bdd->query("SELECT id FROM articles ORDER BY id DESC LIMIT 0,1");
         $req->execute();
@@ -43,7 +43,7 @@ class ArticleManager extends ModelManager
         return $article['id'];
     }
 
-/**modification d'un chapitre */
+    /**modification d'un chapitre */
     public function update($titre, $contenu,$id)
     {
         $art = $this->bdd->prepare('UPDATE articles SET titre = ?, contenu= ?, date_modif= NOW() WHERE id = ?');
@@ -52,7 +52,7 @@ class ArticleManager extends ModelManager
         return $art;
     }
 
-/**suppression d'un chapitre */
+    /**suppression d'un chapitre */
     public function delete($id)
     {
         $req = $this->bdd->prepare('DELETE FROM articles WHERE id=?');
