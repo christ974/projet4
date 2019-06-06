7a4722b76741d2656a6a4bb2e86b12d83335db2c
Garreau christèle <christele.garreau@sfr.fr>
2019-06-06 14:03:55 +0400

    Mise à jour du nom de certaines fonctions

--- app/routes.php
+++ app/routes.php
@@ -7,37 +7,52 @@ $controllerFrontEnd = new ControllerFrontEnd();
 
 /*CONTROLLER FRONTEND*/
 
-/*ACCUEIL*/
+/*ACCUEIL & liste des chapitres avec lien lecture + commentaires*/
 $router->map('GET', '/',function () use ($controllerFrontEnd){
     $controllerFrontEnd->listArticles();
 } );
 
-/*CHAPITRE*/
+/*CHAPITRE permet la lecture d'un chapitre choisi + commentaires*/
 $router->map('GET', '/article/[i:id]', function($id) use ($controllerFrontEnd){
     $controllerFrontEnd->article($id);
 });
+
 /**permet de poster un commentaire */
 $router->map('POST', '/article/[i:id]', function($id) use ($controllerFrontEnd){
-    $controllerFrontEnd->addComment($id); });
+    $controllerFrontEnd->addComment($id); 
+});
 
-    /*AUTEUR*/
+/**permet de signaler un commentaire */
+ $router->map('POST','/comments/signaler/[i:id]',function($id) use($controllerFrontEnd){
+    $controllerFrontEnd->reportComment($id);
+});
+
+/*AUTEUR*/
 $router->map('GET', '/auteur', 'auteur', 'auteur');
 
+/*login et entrée dans page administration */
 $router->map('POST','/login',function() use($controllerFrontEnd){
     $controllerFrontEnd->login();
+});
+
+/*CONTACT par mail*/
+$router->map('GET', '/contact', 'contact', 'contact');
+$router->map('POST','/contact', function () use ($controllerFrontEnd){
+    $controllerFrontEnd->sendingMail();
 });
 
 /*ANNEXES*/
 $router->map( 'GET', '/mentions_legales', 'mentions_legales', 'mentions_legales');
 $router->map( 'GET', '/politiqueDonneesPerso', 'politiqueDonneesPerso', 'politiqueDonneesPerso');
 
-/*------ADMINISTRATION CHAPITRES & COMMENTAIRES-----*/
-$router->map('GET','/administration','administration');
+/*------LOGIN CHAPITRES & COMMENTAIRES-----*/
+$router->map('GET','/login','login');
 
 /*CONTROLLER BACKEND*/
 $controllerBackEnd = new ControllerBackEnd();
 
-/*ADMINISTRATION - EDIT*/
+
+/*ADMINISTRATION - administration du site*/
 $router->map('GET','/edit',function() use ($controllerBackEnd){
     $controllerBackEnd->retourEdit();
 });
@@ -46,7 +61,9 @@ $router->map('POST', '/edit', 'edit', 'edit');
 
 /*----------ADMINISTRATION CHAPITRES------------*/
 /*CREATION D UN CHAPITRE*/
-$router->map('GET', '/creer', 'creer', 'creer');
+$router->map('GET', '/creer', function() use ($controllerBackEnd){
+    $controllerBackEnd->formArticle();
+});
 /**permet de poster un article */
 $router->map('POST', '/creer', function() use ($controllerBackEnd){
     $controllerBackEnd->addArticle();
@@ -70,13 +87,23 @@ $router->map('POST','/supprimer/[i:id]',function($id) use($controllerBackEnd){
 });
 
 /*----------ADMINISTRATION COMMENTAIRES------------*/
+
 /*LISTE COMMENTAIRES SIGNALES*/
 $router->map('GET','/commentaires', function() use ($controllerBackEnd){
     $controllerBackEnd->getCom();
 });
-/*SUPPRIMER COMMENTAIRE SIGNALER */
-$router->map('POST','/',function() use($controllerBackEnd){
-    $controllerBackEnd->supprimerCommentSignaler();
+/*SUPPRIMER COMMENTAIRE SIGNALE */
+$router->map('POST','/commentaires-supprimer/[i:id]',function($id) use($controllerBackEnd){
+     $controllerBackEnd->deleteComments($id);
+});
+/*APPROUVE COMMENTAIRE SIGNALE */
+$router->map('POST','/commentaires-approuver/[i:id]',function($id) use($controllerBackEnd){
+    $controllerBackEnd->approvedComment($id);
 });
 
+
+/**déconnection de l'administration */
+$router->map('POST','/logout',function() use($controllerFrontEnd){
+    $controllerFrontEnd->logout();
+});
 
